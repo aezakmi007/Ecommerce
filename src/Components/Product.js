968e7a87c68813458cc9a7cdfb0252b88407158a
aezakmi007 <abdullahfarhan910@gmail.com>
2021-06-24 14:16:14 +0530

    Added quatity functionality

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Product({ name, price, inStock, currency, image }) {
+  const [quantity, setQuantity] = useState(0);
+
+  const increase = () => {
+    setQuantity(prev => prev + 1);
+  };
+  const decrease = () => {
+    if (quantity !== 0) {
+      setQuantity(prev => prev - 1);
+    }
+  };
+
   return (
     <div>
       <img src={image} alt="xyz" />
-      <h3>{name}</h3>
+      <h3>Product Name : {name}</h3>
       <h4>
-        {price}
+        Amount :{price}
         {currency}
       </h4>
-      <h4>{inStock ? 'Yes' : 'No'}</h4>
+      <h4>In Stock : {inStock ? 'Yes' : 'No'}</h4>
+      <p> Qunatity : {quantity}</p>
+      <button type="button" disabled={!inStock} onClick={increase}>
+        increase
+      </button>
+      <button type="button" disabled={!inStock} onClick={decrease}>
+        decrease
+      </button>
+      <button type="button" disabled={!inStock}>
+        Add to cart
+      </button>
     </div>
   );
 }
