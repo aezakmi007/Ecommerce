34df01273e82e886e4b5105e93b3997cf48910b8
aezakmi007 <abdullahfarhan910@gmail.com>
2021-07-06 21:58:08 +0530

    Finised Project with all functionalities

--- src/Components/CheckOutProducts.js
+++ src/Components/CheckOutProducts.js
@@ -1,24 +1,52 @@
-import React, { useState } from 'react';
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-destructuring */
+import React, { useEffect, useState } from 'react';
 import useCart from '../context/CartContext';
 import prod from '../data/products';
 
 function CheckOutProducts({ name, image, id, price }) {
   console.log(id);
   console.log(name);
-  const { addTotal, decreaseTotal, reduceCart, decreaseNoOfItems } = useCart();
+  const {
+    addTotal,
+    decreaseTotal,
+    reduceCart,
+    decreaseNoOfItems,
+    changeNoOfItems,
+  } = useCart();
 
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(
+    localStorage.getItem('quantityData')
+      ? JSON.parse(localStorage.getItem('quantityData')).length !== 0
+        ? JSON.parse(localStorage.getItem('quantityData')).find(x => x.i === id)
+            .quant
+        : 1
+      : 1
+  );
+
+  const removeQuantity = y => {
+    let data = JSON.parse(localStorage.getItem('quantityData'));
+    data = data.filter(x => x.i !== y);
+    if (data.length === 0) {
+      localStorage.removeItem('quantityData');
+    } else {
+      localStorage.setItem('quantityData', JSON.stringify(data));
+    }
+  };
 
   const onIncrease = val => {
     setQuantity(prev => prev + 1);
     addTotal(prod.find(x => x.id === val).price);
-    // changeNoOfItems();
+    changeNoOfItems();
   };
 
   const onDecrease = val => {
     if (quantity > 0) {
       setQuantity(prev => prev - 1);
       decreaseTotal(prod.find(x => x.id === val).price);
+      decreaseNoOfItems(1);
       console.log(val);
     }
   };
@@ -26,13 +54,52 @@ function CheckOutProducts({ name, image, id, price }) {
   const onRemove = val => {
     if (quantity === 0) {
       reduceCart(prod.find(x => x.id === String(val)));
-      decreaseNoOfItems();
+      decreaseNoOfItems(quantity);
+      removeQuantity(id);
     } else {
       reduceCart(prod.find(x => x.id === val));
       decreaseTotal(prod.find(x => x.id === val).price * quantity);
-      decreaseNoOfItems();
+      decreaseNoOfItems(quantity);
+      removeQuantity(id);
     }
   };
+  // useEffect(() => {
+  //   const data = [];
+  //   localStorage.setItem('quantityData', JSON.stringify(data));
+  // });
+
+  useEffect(() => {
+    const arr = {
+      i: `${id}`,
+      quant: quantity,
+    };
+    if (localStorage.hasOwnProperty('quantityData') === true) {
+      if (JSON.parse(localStorage.getItem('quantityData')).length !== 0) {
+        let getData = JSON.parse(localStorage.getItem('quantityData'));
+        const val = getData.find(x => x.i === id);
+
+        if (val) {
+          getData.forEach(x => {
+            if (x.i === id) {
+              x.quant = quantity;
+              localStorage.setItem('quantityData', JSON.stringify(getData));
+            }
+          });
+        } else {
+          getData = getData.concat(arr);
+          localStorage.setItem('quantityData', JSON.stringify(getData));
+        }
+      } else {
+        let getData = JSON.parse(localStorage.getItem('quantityData'));
+        getData = getData.concat(arr);
+        localStorage.setItem('quantityData', JSON.stringify(getData));
+      }
+    } else {
+      const a = [];
+      a.push(arr);
+      localStorage.setItem('quantityData', JSON.stringify(a));
+    }
+  }, [quantity]);
 
   return (
     <div>
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,11 +1,24 @@
-import React, { useState, createContext, useContext } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, createContext, useContext, useEffect } from 'react';
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [total, setTotal] = useState(0);
-  const [noOfItems, setNoOfItems] = useState(0);
-  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(
+    localStorage.getItem('total')
+      ? JSON.parse(localStorage.getItem('total'))
+      : 0
+  );
+  const [noOfItems, setNoOfItems] = useState(
+    localStorage.getItem('noOfItems')
+      ? JSON.parse(localStorage.getItem('noOfItems'))
+      : 0
+  );
+  const [items, setItems] = useState(
+    localStorage.getItem('items')
+      ? JSON.parse(localStorage.getItem('items'))
+      : []
+  );
 
   const addTotal = value => {
     setTotal(prev => prev + value);
@@ -13,9 +26,13 @@ export const CartProvider = ({ children }) => {
 
   const addItems = prod => {
     setItems(prev => [...prev, prod[0]]);
+    // const key =
+    // localStorage.setItem(item = items)
   };
   const decreaseTotal = value => {
+    // const price = 'price';
     setTotal(prev => prev - value);
+    // localStorage.setItem((price, JSON.stringify(total)));
   };
   const reduceCart = value => {
     const arr = [...items];
@@ -26,9 +43,19 @@ export const CartProvider = ({ children }) => {
   const changeNoOfItems = () => {
     setNoOfItems(prev => prev + 1);
   };
-  const decreaseNoOfItems = () => {
-    setNoOfItems(prev => prev - 1);
+  const decreaseNoOfItems = val => {
+    setNoOfItems(prev => prev - val);
   };
+  useEffect(() => {
+    localStorage.setItem('total', total);
+    localStorage.setItem('noOfItems', noOfItems);
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items, noOfItems, total]);
+  // useEffect(() => {
+  //   setItems(JSON.parse(localStorage.getItem('items')));
+  //   setNoOfItems(JSON.parse(localStorage.getItem('noOfItems')));
+  //   setTotal(JSON.parse(localStorage.getItem('total')));
+  // }, []);
   return (
     <CartContext.Provider
       value={{
