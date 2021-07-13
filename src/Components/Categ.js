cc19fc4f75dd9164fa597bb1795f8d1350d8d99c
aezakmi007 <abdullahfarhan910@gmail.com>
2021-07-13 22:13:21 +0530

    Added some styling to the category page

--- src/Components/Categ.js
+++ src/Components/Categ.js
@@ -7,18 +7,18 @@ import '../Styles/CategoryPage.css';
 function Categ({ name, desc, id }) {
   return (
     <div className="card">
-      <img
-        className={
-          'card__image ' + (name === 'Headphones' ? 'headPhone__image' : '')
-        }
-        src={
-          name === 'Keyboards'
-            ? 'https://mechanicalkeyboards.com/shop/images/products/large_2790_SakuraTKL_1.jpg'
-            : 'https://images-na.ssl-images-amazon.com/images/I/71BKQhFzDmL._AC_SY355_.jpg'
-        }
-        alt="xyz"
-      />
-      <div className="card___content">
+      <div className="image__div">
+        <img
+          className="card__image "
+          src={
+            name === 'Keyboards'
+              ? 'https://mechanicalkeyboards.com/shop/images/products/large_2790_SakuraTKL_1.jpg'
+              : 'https://images-na.ssl-images-amazon.com/images/I/71BKQhFzDmL._AC_SY355_.jpg'
+          }
+          alt="xyz"
+        />
+      </div>
+      <div className="card__content">
         <h1>{name}</h1>
         <p>{desc}</p>
         <div className="card__info">
